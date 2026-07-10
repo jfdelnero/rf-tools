94a413c098d99b14a5ba174d6861ecbd9f0173a5
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2026-07-10 09:34:54 +0200

    forecast frame generation : areaid option added.

--- src/pocsag/starmeteo.c
+++ src/pocsag/starmeteo.c
@@ -367,13 +367,14 @@ int main(int argc, char* argv[])
 		printf("%s -checksum     (Update checksum with \"-encode\")\n",argv[0]);
 		printf("%s -curtime       Generate current date/hour frame\n",argv[0]);
 		printf("%s -forecast:[LowTemp],[HighTemp],[MainPicto],[Picto_2],[Picto_3],[Picto_4],[Picto_5]\n",argv[0]);
+		printf("%s -areaid:[idcode]\n",argv[0]);
 
 		printf("%s -quiet\n",argv[0]);
 		printf("%s -verbose\n",argv[0]);
 		printf("\n");
 		printf("Example: %s -decode ../previsions_ok/*.txt\n",argv[0]);
 		printf("Example: %s -curtime -quiet\n",argv[0]);
-		printf("Example: %s -forecast:-10,40,1,2,3,4,5 -forecast:-11,41,6,7,8,9,10 -forecast:-12,42,11,12,13,14,15 -forecast:-13,43,16,17,18,19,20 -quiet\n",argv[0]);
+		printf("Example: %s -forecast:-10,40,1,2,3,4,5 -forecast:-11,41,6,7,8,9,10 -forecast:-12,42,11,12,13,14,15 -forecast:-13,43,16,17,18,19,20 -areaid:75 -quiet\n",argv[0]);
 		exit(0);
 	}
 
@@ -655,6 +656,11 @@ int main(int argc, char* argv[])
 		int departement;
 
 		departement = 75;
+
+		if(isOption(argc, argv,"areaid",(char*)tmp_str, NULL) )
+		{
+			departement = atoi(tmp_str);
+		}
 
 		genfrm = calloc(sizeof(frame)*1,1);
 		if(!genfrm)
